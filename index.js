66aba63bf7d653b8a0a37ce243a88d65d136d598
Pedro <pedroquinonez0@gmail.com>
2015-11-23 14:32:59 -0700

    Primer commit, principio de proyecto.

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static(__dirname));
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+// New socket connection
+io.on('connection', function(socket){
+  // Socket connection successful
+  console.log('a user connected '+ socket.id);
+});
+
+
+
+http.listen(process.env.PORT || 5000, function(){
+  console.log('listening on *:5000');
+});
